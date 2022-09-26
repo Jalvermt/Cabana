fe940dd4f03895fa5a314fe649b486e181cf181f
jalverGlobaltek <90347169+jalverGlobaltek@users.noreply.github.com>
2022-09-25 23:06:05 -0500

    Configuracion HTML se agrega messagge y creacion de JS Message

--- myScritMessage.js
+++ myScritMessage.js
@@ -1,16 +1,16 @@
 function leerMessage(){
     //FUNCION GET
         $.ajax({    
-            url : 'https://gcb03291783e9d6-q8y7hi9e9jd8s635.adb.mx-queretaro-1.oraclecloudapps.com/ords/admin/cabin/cabin',
+            url : 'https://gcb03291783e9d6-q8y7hi9e9jd8s635.adb.mx-queretaro-1.oraclecloudapps.com/ords/admin/message/message',
             type : 'GET',
             dataType : 'json',
     
-            success : function(Cabañas) {
-                   let cs=Cabañas.items;
-                   $("#listaCabañas").empty();
+            success : function(Message) {
+                   let cs=Message.items;
+                   $("#listaMessage").empty();
                    for(i=0;i<cs.length;i++){
-                       $("#listaCabañas").append(cs[i].id+" <b>"+cs[i].brand+"</b> "+cs[i].rooms+" </b>"+cs[i].category_id+" </b>"+cs[i].name);
-                       $("#listaCabañas").append("<button onclick='borrarCabañas("+cs[i].id+")'>Borrar cb</button><br>");
+                       $("#listaMessage").append(cs[i].id+" <b>"+cs[i].messagetext );
+                       $("#listaMessage").append("<button onclick='borrarMessage("+cs[i].id+")'>Borrar cb</button><br>");
                    
                    }
             },
