a8f60e13e9b3d219883fdbba62736d1d08fd9909
jalverGlobaltek <90347169+jalverGlobaltek@users.noreply.github.com>
2022-09-28 22:35:51 -0500

    DELETE para mensaje

--- myScritMessage.js
+++ myScritMessage.js
@@ -10,7 +10,7 @@ function leerMessage(){
                    $("#listaMessage").empty();
                    for(i=0;i<cs.length;i++){
                        $("#listaMessage").append(cs[i].id+" <b>"+cs[i].messagetext );
-                       $("#listaMessage").append("<button onclick='borrarMessage("+cs[i].id+")'>Borrar cb</button><br>");
+                       $("#listaMessage").append("<button onclick='borrarMessages("+cs[i].id+")'>Borrar MSG</button><br>");
                    
                    }
             },
@@ -54,6 +54,7 @@ function guardarMessages() {
 	    }
 	});
 }
+
 function editarMessages(){
 	let idMessage=$("#idMensaje").val();
 	let mensaje=$("#mensaje").val();
@@ -61,8 +62,6 @@ function editarMessages(){
 	let data={
 		id:idMessage,
 		messagetext:mensaje
-		
-		
 		};
 
 	let dataToSend=JSON.stringify(data);
@@ -74,7 +73,33 @@ function editarMessages(){
 	    data:dataToSend,
 	    contentType:'application/json',
 	    success : function(pepito) {
-			$("#idMessage").val("");
+			$("#idMensaje").val("");
+			$("#mensaje").val("");
+	    },
+	    error : function(xhr, status) {
+	   //     alert('ha sucedido un problema');
+	    },
+	    complete: function(){
+	    	leerMessage();
+	    }
+	});
+
+}
+
+function borrarMessages(idCliente){
+	let data={
+		id:idMessage
+	};
+	let dataToSend=JSON.stringify(data);
+	//console.log(dataToSend);
+	$.ajax({    
+	    url :'https://gcb03291783e9d6-q8y7hi9e9jd8s635.adb.mx-queretaro-1.oraclecloudapps.com/ords/admin/message/message',
+	    type : 'DELETE',
+	 //   dataType : 'json',
+	    data:dataToSend,
+	    contentType:'application/json',
+	    success : function(pepito) {
+			$("#idMensaje").val("");
 			$("#mensaje").val("");
 	    },
 	    error : function(xhr, status) {
