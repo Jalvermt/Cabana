394c3e18efbc6080d8d0512f5866970fa3b373a3
jalverGlobaltek <90347169+jalverGlobaltek@users.noreply.github.com>
2022-09-28 21:16:55 -0500

    PUT para mensaje

--- myScritMessage.js
+++ myScritMessage.js
@@ -19,3 +19,70 @@ function leerMessage(){
             }
         });
     }
+	
+function guardarMessages() {
+	let idMessage=$("#idMensaje").val();
+	let mensaje=$("#mensaje").val();
+
+	let data={
+		id:idMessage,
+		messagetext:mensaje
+		
+		
+		};
+
+	let dataToSend=JSON.stringify(data);
+	//console.log(dataToSend);
+
+
+	$.ajax({    
+	    url : 'https://gcb03291783e9d6-q8y7hi9e9jd8s635.adb.mx-queretaro-1.oraclecloudapps.com/ords/admin/message/message',
+	    type : 'POST',
+	 //   dataType : 'json',
+	    data:dataToSend,
+	    contentType:'application/json',
+	    success : function(pepito) {
+	   		$("#idMessage").val("");
+			$("#mensaje").val("");
+			
+	    },
+	    error : function(xhr, status) {
+	   //     alert('ha sucedido un problema');
+	    },
+	    complete: function(){
+	    	leerMessage();
+	    }
+	});
+}
+function editarMessages(){
+	let idMessage=$("#idMensaje").val();
+	let mensaje=$("#mensaje").val();
+
+	let data={
+		id:idMessage,
+		messagetext:mensaje
+		
+		
+		};
+
+	let dataToSend=JSON.stringify(data);
+	//console.log(dataToSend);
+	$.ajax({    
+	    url : 'https://gcb03291783e9d6-q8y7hi9e9jd8s635.adb.mx-queretaro-1.oraclecloudapps.com/ords/admin/message/message',
+	    type : 'PUT',
+	 //   dataType : 'json',
+	    data:dataToSend,
+	    contentType:'application/json',
+	    success : function(pepito) {
+			$("#idMessage").val("");
+			$("#mensaje").val("");
+	    },
+	    error : function(xhr, status) {
+	   //     alert('ha sucedido un problema');
+	    },
+	    complete: function(){
+	    	leerMessage();
+	    }
+	});
+
+}
